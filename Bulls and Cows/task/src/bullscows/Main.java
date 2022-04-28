12cdb68aff3e1a3a1daa507a5bbcc04a10c139c4
Junior <94207328+Makif123@users.noreply.github.com>
2022-04-28 06:47:34 +0100

    First Task

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,7 +1,21 @@
 package bullscows;
 
+import java.awt.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("The secret code is prepared: ****.\n" +
+                "\n" +
+                "Turn 1. Answer:\n" +
+                "1234\n" +
+                "Grade: None.\n" +
+                "\n" +
+                "Turn 2. Answer:\n" +
+                "9876\n" +
+                "Grade: 4 bulls.\n" +
+                "Congrats! The secret code is 9876.");
     }
+
+    ;
 }
