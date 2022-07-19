c0a2f23ba295a2c859aa263f9fa57fe315ad0db1
Junior <94207328+Makif123@users.noreply.github.com>
2022-07-19 14:34:55 +0100

    Hopefull it's finished for 4/grade

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -12,18 +12,23 @@ public class Main {
     int count = 1;
 
     public static void main(String[] args) {
+        int count = 1;
+        Long first;
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Please, enter the secret code's length:");
+
+
         length = scanner.nextInt();
+
         if (length > 10) {
             System.out.println("Error: can't generate a secret number with a length of " + length + " because there aren't enough unique digits.");
             System.exit(0);
         }
-        System.out.println("Okay, let's start a game!");
+
 
         long pseudoRandomNumber = 0;
         Long pseudoRandomNumber1 = null;
+
 
         do {
             pseudoRandomNumber = System.nanoTime();
@@ -36,101 +41,113 @@ public class Main {
 
         } while (!(isUnique(pseudoRandomNumber1)));
 
-
-        System.out.println("The random secret number is " + pseudoRandomNumber1 + ".");
-
+        String pseudoRandomNumber1String = pseudoRandomNumber1.toString();
 
         ArrayList<Integer> guessDigitList = new ArrayList<>();
         int cows = 0;
         int bulls = 0;
-        Integer digit = 0;
-        Integer first = scanner.nextInt();
-        ArrayList<Integer> arrs = convertFromStringToArrayList(first);
+        Long digit = pseudoRandomNumber1;
+        System.out.println("Okay, let's start a game!");
+        do {
 
 
-        ArrayList<Integer> secretCode = new ArrayList<>();
-        secretCode.addAll(Arrays.asList(9, 3, 0, 5));
+//            System.out.println("pseudoRandomNumber1 = " + pseudoRandomNumber1);
 
-        for (int i = 0; i < length; i++) {
 
-            digit = arrs.get(i);
-            guessDigitList.add(digit);
-            if (secretCode.contains(digit)) {
-                if (guessDigitList.indexOf(digit) == guessDigitList.lastIndexOf(digit)) {
-                    if (guessDigitList.indexOf(digit) != secretCode.indexOf(digit)) {
-                        cows++;
-                    }
-                } else {
-                    if (i >= 1) {
-                        if (guessDigitList.get(i - 1).equals(guessDigitList.get(i))) {
-                            if (guessDigitList.lastIndexOf(digit) != secretCode.lastIndexOf(digit)) {
-                                cows++;
+            System.out.println("Turn " + count + ":");
+
+            first = scanner.nextLong();
+            ArrayList<Long> secretCode = convertFromStringToArrayList(pseudoRandomNumber1);
+            ArrayList<Long> arrs = convertFromStringToArrayList(first);
+            ArrayList<Long> guessDigitList1 = new ArrayList<>();
+
+
+
+            for (int i = 0; i < length; i++) {
+
+                digit = arrs.get(i);
+                guessDigitList1.add(digit);
+                if (secretCode.contains(digit)) {
+                    if (guessDigitList1.indexOf(digit) == guessDigitList1.lastIndexOf(digit)) {
+                        if (guessDigitList1.indexOf(digit) != secretCode.indexOf(digit)) {
+                            cows++;
+                        }
+                    } else {
+                        if (i >= 1) {
+                            if (guessDigitList1.get(i - 1).equals(guessDigitList1.get(i))) {
+                                if (guessDigitList1.lastIndexOf(digit) != secretCode.lastIndexOf(digit)) {
+                                    cows++;
+                                }
                             }
                         }
                     }
-                }
 
-                if (guessDigitList.lastIndexOf(digit) == secretCode.indexOf(digit)) {
-                    bulls++;
+                    if (guessDigitList1.lastIndexOf(digit) == secretCode.indexOf(digit)) {
+                        bulls++;
+                    }
                 }
             }
-        }
 
-        if (cows > 0 && bulls > 0) {
+            if (cows > 0 && bulls > 0) {
 
-            System.out.print("Grade: " + bulls + " bull(s) and " + cows + " cow(s). The secret code is ");
-            arrayListPrint(secretCode);
-            System.out.print(".");
-            System.exit(0);
-
-        }
-        if (bulls > 0) {
-
-            System.out.print("Grade: " + bulls + " bull(s). The secret code is ");
-            arrayListPrint(secretCode);
-            System.out.print(".");
-            System.exit(0);
-        }
-        if (cows > 0) {
-
-            System.out.print("Grade: " + cows + " cow(s). The secret code is ");
-            arrayListPrint(secretCode);
-            System.out.print(".");
-            System.exit(0);
-        }
-
-        if (cows == 0 && bulls == 0) {
-
-            System.out.print("Grade: None. The secret code is ");
-            arrayListPrint(secretCode);
-            System.out.print(".");
-            System.exit(0);
-        }
+                System.out.println("Grade: " + bulls + " bull(s) and " + cows + " cow(s)..");
+//
 
 
+            }
+            if (bulls > 0) {
+
+                System.out.println("Grade: " + bulls + " bull(s)..");
+
+
+
+
+            }
+            if (cows > 0) {
+
+                System.out.println("Grade: " + cows + " cow(s)..");
+
+
+
+            }
+
+            if (cows == 0 && bulls == 0) {
+
+                System.out.print("Grade: None.");
+//
+
+            }
+            count++;
+
+            cows = 0;
+            bulls = 0;
+        } while (!(pseudoRandomNumber1.equals(first)));
+        System.out.println("Congratulations! You guessed the secret code.");
     }
 
     public static String a = "";
 
-    public static void arrayListPrint(ArrayList<Integer> secretCode) {
-        for (Integer integer : secretCode) {
+    public static void arrayListPrint(ArrayList<Long> secretCode) {
 
-            a += "" + integer;
+
+        for (Long along : secretCode) {
+
+            a += "" + along;
         }
         System.out.print(a);
         a = "";
 
     }
 
-    public static ArrayList<Integer> convertFromStringToArrayList(Integer first) {
+    public static ArrayList<Long> convertFromStringToArrayList(Long first) {
 
 
-        Integer firstDigit = first / 1000;
-        Integer secondDigit = (first / 100) % 10;
-        Integer thirdDigit = (first / 10) % 10;
-        Integer fourthDigit = first % 10;
+        Long firstDigit = first / 1000;
+        Long secondDigit = (first / 100) % 10;
+        Long thirdDigit = (first / 10) % 10;
+        Long fourthDigit = first % 10;
 
-        ArrayList<Integer> arrs = new ArrayList<>();
+        ArrayList<Long> arrs = new ArrayList<>();
         arrs.addAll(Arrays.asList(firstDigit, secondDigit, thirdDigit, fourthDigit));
 
 
